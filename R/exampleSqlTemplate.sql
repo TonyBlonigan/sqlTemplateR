90e3ea077b508a9d3063805b038c88a22340093c
Anthony Blonigan <A3BW9ZZ@usac.mmm.com>
2018-06-04 13:17:30 -0500

    updated vignette examples

--- R/exampleSqlTemplate.sql
+++ R/exampleSqlTemplate.sql
@@ -1,6 +1,6 @@
 --This is used in the @examples in query.R > generateSQL
 SELECT
-NVL(cust.S_DUNS_NBR, cust.CUST_KEY) CUST_KEY
+cust.CUST_KEY CUST_KEY
 --,naics.NAICS_DESC
 --,MAX(prod.ITEM_DESC) ITEM_DESC
 
@@ -8,16 +8,16 @@ NVL(cust.S_DUNS_NBR, cust.CUST_KEY) CUST_KEY
 
 FROM SALES sls
 INNER JOIN CUSTOMER cust
-	ON sls.CUST_KEY = NVL(cust.S_DUNS_NBR, cust.CUST_KEY)
+	ON sls.CUST_KEY = cust.CUST_KEY
 INNER JOIN PRODUCT prod
 	ON sls.PROD_KEY = prod.PROD_KEY
 INNER JOIN N_AM_CLASSIFICATION naics
 	ON cust.NAICS_CODE = naics.NAICS_CODE
 
 WHERE CAST(sls.SLS_YR || '-' || LPAD(CAST(sls.SLS_MNTH_INT AS CHAR), 2, '0') || '-' || '01' AS DATE) > ADD_MONTHS(DATE, - 14)
-AND (cust.S_DUNS_NBR IS NOT NULL OR cust.CUST_CD = 'D')
+AND (cust.S_CUST_KEY IS NOT NULL OR cust.CUST_CD = 'D')
 --<prodFilter>
 GROUP BY
-NVL(cust.S_DUNS_NBR, cust.CUST_KEY)
+cust.CUST_KEY
 
 --,naics.NAICS_DESC
